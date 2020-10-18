f75e4d218d693df979ee5c55349cb4156e296c10
Paulo Roberto Rosa <paulo.roberto@geekhunter.com.br>
2020-10-18 14:28:12 -0300

    feat: done update service to edit users.

---
+++ api/app/services/user/update_service.rb
@@ -0,0 +1,33 @@
+class User
+  class UpdateService
+    attr_reader :params, :user
+
+    def initialize(params:)
+      @params = params
+    end
+
+    def call
+      return nil if params.nil? || params[:id].nil?
+      update_user if found_user.present?
+    end
+
+    private
+
+    def update_user
+      user.name = params[:name] if params[:name].present?
+      user.email = params[:email] if params[:email].present?
+      user.birthday = params[:birthday] if params[:birthday].present?
+      user.gender = params[:gender] if params[:gender].present?
+      user.save ? user : nil
+    end
+
+    def found_user
+      begin
+        @user = User.find(params[:id])
+      rescue ActiveRecord::RecordNotFound
+        @user = nil
+      end
+    end
+
+  end
+end
