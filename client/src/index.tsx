769c653b746a5ca12806536a083bbbb9a7a083dd
Paulo Roberto Rosa <paulo.roberto@geekhunter.com.br>
2020-10-19 11:22:25 -0300

    feat: included ant design framework on project

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
-import './index.css';
 import store from './store';
 import * as serviceWorker from './serviceWorker';
 import AppRouter from './RouterConfig';
+import './index.css';
 
 ReactDOM.render(
   <React.StrictMode>
