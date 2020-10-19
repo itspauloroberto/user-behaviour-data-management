458b6d366e09127929ce750ae5950ed849d8bf1c
Paulo Roberto Rosa <paulo.roberto@geekhunter.com.br>
2020-10-19 16:06:47 -0300

    feat: enabled CORS on api open to all for now.

--- api/config/application.rb
+++ api/config/application.rb
@@ -11,6 +11,18 @@ module Ubdm
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
+    # Only loads a smaller set of middleware suitable for API only apps.
+    # Middleware like session, flash, cookies can be added back manually.
+    # Skip views, helpers and assets when generating a new resource.
+    config.api_only = true
+
+    config.middleware.insert_before 0, Rack::Cors do
+      allow do
+        origins '*'
+        resource '*', :headers => :any, :methods => [:get, :patch, :post, :options]
+      end
+    end
+
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
